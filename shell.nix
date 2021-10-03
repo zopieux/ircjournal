c72b1f1c3fc83f41d4125ae58024ee4023f3f85c
Alexandre Macabies <web+oss@zopieux.com>
2021-10-03 04:02:26 +0200

    Update shell.nix with new dependencies.

--- shell.nix
+++ shell.nix
@@ -5,13 +5,41 @@ let
     extensions = [ "rust-src" ];
     targets = [ "x86_64-unknown-linux-gnu" ];
   });
+
+  # TODO: dart-sass is not packaged :(
+  dart-sass-bin = let version = "1.42.1"; in
+    pkgs.stdenv.mkDerivation
+      rec {
+        pname = "dart-sass";
+        inherit version;
+        system = "x86_64-linux";
+        isExecutable = true;
+        src = pkgs.fetchurl {
+          sha256 = "1rxswaxly1p829zh6sfbif3fpw5shnkmf604mi1x4v11v4ra8f6b";
+          url = builtins.concatStringsSep "/" [
+            "https://github.com"
+            "sass/dart-sass/releases/download"
+            "${version}/dart-sass-${version}-linux-x64.tar.gz"
+          ];
+        };
+        phases = "unpackPhase installPhase";
+        installPhase = ''
+          mkdir -p $out/bin
+          cp -r . $out
+          ln -s $out/sass $out/bin/sass
+        '';
+      };
+
 in
 pkgs.mkShell {
-  buildInputs = with pkgs; [
-    rust
-    postgresql
-    openssl # required by sqlx_macros: libssl, libcrypto
-  ];
+  buildInputs = with pkgs;
+    [
+      rust
+      postgresql
+      openssl # required by sqlx_macros: libssl, libcrypto
+      nodePackages.typescript
+      dart-sass-bin
+    ];
 
   SQLX_OFFLINE = "true";
   RUST_LOG = "debug";
