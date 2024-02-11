730450f727bd85dd293191e4831fc62adbceeb0d
Alexandre Macabies <web+oss@zopieux.com>
2024-02-11 15:25:39 +0100

    Add nix flake.

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "ircjournal, a lightweight, fast, standalone IRC log viewer for the web, with real-time log ingestion.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs { inherit system; };
+    in rec
+    {
+      packages = rec {
+        ircjournal = pkgs.rustPlatform.buildRustPackage {
+          pname = "ircjournal";
+          version = "local";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          cargoLock.outputHashes."maud-0.23.0" = "sha256-1U8KWxFwvxxxtrgW19NMwkbf0tRG4PEAYvdOzmaFaiM=";
+          SQLX_OFFLINE = 1;
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            nodePackages.typescript
+            sassc
+          ];
+          buildInputs = with pkgs; [ openssl ];
+        };
+        default = ircjournal;
+      };
+      devShells.default = pkgs.mkShell {
+        shellHook = ''
+          echo 'To create a test DB, use: export DATABASE_URL=$(pg_tmp -w 0 -d /tmp/ircjournal-pg)'
+        '';
+        RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+        inputsFrom = with packages; [ ircjournal ];
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+          ephemeralpg
+          clippy
+          sqlx-cli
+        ];
+      };
+    });
+}
